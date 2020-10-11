6776bf92cf80584797bde14697b19926d18d5ed0
sqz269 <yin2690855381@gmail.com>
2020-10-11 17:01:00 -0500

    Minor changes

--- PlaylistShuffler/site/views.py
+++ PlaylistShuffler/site/views.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, make_response, Response
 
 site = Blueprint("site", __name__, template_folder="template", static_folder="static", static_url_path="/site/static")
 
 @site.route("/")
 def index():
+    resp: Response = make_response(render_template('index.html'))
+    if 'apiKey' not in request.cookies:
+        resp.set_cookie('apiKey', '')
+    if 'clientId' not in request.cookies:
+        resp.set_cookie('clientId', '')
     return render_template("index.html")
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from PlaylistShuffler import app
 
 if __name__ == "__main__":
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run("192.168.29.197", 8080)
+    app.run("localhost", 80)
