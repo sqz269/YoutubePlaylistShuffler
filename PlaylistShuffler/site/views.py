41acde51c56f48daf382394e68053fb465ebfd4b
sqz269 <yin2690855381@gmail.com>
2020-10-12 15:48:22 -0500

    Improved player layout

--- PlaylistShuffler/site/views.py
+++ PlaylistShuffler/site/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, make_response, Response
 
 site = Blueprint("site", __name__, template_folder="template", static_folder="static", static_url_path="/site/static")
 
+
 @site.route("/")
 def index():
     resp: Response = make_response(render_template('index.html'))
